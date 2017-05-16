69bc164065271fc070f0dcce8692a932c5369c60
yureev <y.beletskiy@gmail.com>
2017-05-16 18:38:02 +0300

    production

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
     // вычислить результат
     equal.on('click', function () {
         // 1000000000/100000000 для нормального вывода 0.1+0.2(и других)
-        display.val( Math.round(eval(display.val())* 10000000000) / 10000000000);
+        display.val( Math.round(eval(display.val())* 1000000000000000) / 1000000000000000);
     });
 
     // удалить последний символ
@@ -28,13 +28,13 @@ $(document).ready(function () {
         display.val( display.val().substring(0, display.val().length-1) );
     });
 
-    // event.type должен быть keypress
+    // разрешенные символы (event.type должен быть keypress)
     getChar = function (event) {
         if (event.which == null) { // IE
             if (event.keyCode > 39 && event.keyCode < 58 && event.keyCode != 44) {
                 return; // цифры и действия
             } else if (event.keyCode == 13 || event.keyCode == 61){
-                display.val( Math.round(eval(display.val())* 10000000000) / 10000000000);
+                display.val( Math.round(eval(display.val())* 1000000000000000) / 1000000000000000);
             }
             return event.preventDefault()
         }
@@ -42,9 +42,15 @@ $(document).ready(function () {
             if (event.which > 39 && event.which < 58 && event.which != 44) {
                 return; // цифры и действия
             } else if (event.which == 13 || event.which == 61){
-                display.val( Math.round(eval(display.val())* 10000000000) / 10000000000);
+                display.val( Math.round(eval(display.val())* 1000000000000000) / 1000000000000000);
             }
             return event.preventDefault();
         }
     };
+
+    // запрет на вставку
+    document.getElementById("display").onpaste = function() {
+        return false;
+    };
+
 });
